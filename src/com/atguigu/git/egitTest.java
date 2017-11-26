1a0624b51b289c9fb9548929cd5a84a925e19ff2
wangzhiweishiwo <wangzhiweishiwo@126.com>
2017-11-26 16:53:16 +0800

    Test3

--- src/com/atguigu/git/egitTest.java
+++ src/com/atguigu/git/egitTest.java
@@ -3,5 +3,10 @@ package com.atguigu.git;
 public class egitTest {
 	public static void main(String[] args) {
 		System.out.println("Hello EGit");
+		System.out.println("Hello EGit");
+		System.out.println("Hello EGit");
+		System.out.println("Hello EGit");
+		System.out.println("Hello EGit");
+		System.out.println("Hello EGit");
 	}
 }
